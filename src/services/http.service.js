489fffb70610f1be0be61ca7d1aedabc2c0318a2
AriGabay <arigabay19988@gmail.com>
2021-06-21 23:10:13 +0300

    add env vars

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,21 +1,18 @@
 const axios = require('axios');
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
+const BASE_URL = process.env.REACT_APP_API_HOST;
 console.log('BASE_URL:', BASE_URL);
 export const httpService = {
   get(endpoint, query) {
     const queryStr = Object.keys(query)
       .map((key) => key + '=' + query[key])
       .join('&');
-    console.log('queryStr:', queryStr);
-    return axios.get(`http://localhost:3030/api/${endpoint}?${queryStr}`).then((response) => response.data);
+    return axios.get(`${BASE_URL}${endpoint}?${queryStr}`).then((response) => response.data);
   },
   post(endpoint, data) {
-    console.log('data:', data);
-    console.log('BASE_URL + endpoint:', BASE_URL + endpoint);
     return axios.post(BASE_URL + endpoint, data).then((res) => res.data);
   },
   put(endpoint, data) {
-    return axios(endpoint, 'PUT', data);
+    return axios.put(endpoint, data);
   },
   delete(endpoint, id) {
     return axios.delete(BASE_URL + endpoint + id);
