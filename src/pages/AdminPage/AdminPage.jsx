9505f4f81eed1cd01bcc239d7013a13d0c609040
AriGabay <arigabay19988@gmail.com>
2023-09-06 01:28:00 +0300

    Update AdminPage.jsx

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -14,6 +14,7 @@ import { eventBus } from '../../services/event-bus';
 import { authService } from '../../services/authService';
 import { GnCategory } from '../../cmps/AdminPage/GannayEyalon/GnCategory/GnCategory';
 import { GnProduct } from '../../cmps/AdminPage/GannayEyalon/GnProduct';
+import { Helmet } from 'react-helmet';
 
 const useStyles = makeStyles(() => ({
   marginLeft: {
@@ -45,130 +46,135 @@ export const AdminPage = () => {
   }, []);
 
   return (
-    Object.keys(userAuth).length && (
-      <Grid mt={2} mr={2}>
-        <Button
-          className={classes.marginLeft}
-          text="קטגוריה"
-          onClick={() => handleClick('Category')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="מחירון"
-          onClick={() => handleClick('Price')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="מחיר למחירון"
-          onClick={() => handleClick('SizePrice')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="מוצר"
-          onClick={() => handleClick('Product')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="הזמנות"
-          onClick={() => handleClick('Orders')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="נעילת תפריט"
-          onClick={() => handleClick('Menu')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="הזמנות על פי תאריך"
-          onClick={() => handleClick('GetOrdersByData')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="הזמנות לתאריך ספציפי"
-          onClick={() => handleClick('OrderByDate')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="קטגוריות - גני איילון"
-          onClick={() => handleClick('GnCategory')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="מוצר - גני איילון"
-          onClick={() => handleClick('GnProduct')}
-        />
-        <Button
-          className={classes.marginLeft}
-          text="ענן תמונות"
-          onClick={() => handleClick('GetCloudinaryDetails')}
-        />
-        {status && status === 'Category' ? (
-          <Grid mt={2}>
-            <AdminPageCategory eventBus={eventBus}></AdminPageCategory>
-          </Grid>
-        ) : null}
-        {status && status === 'Price' ? (
-          <Grid mt={2}>
-            <AdminPagePrice eventBus={eventBus}></AdminPagePrice>
-          </Grid>
-        ) : null}
-        {status && status === 'SizePrice' ? (
-          <Grid mt={2}>
-            <AdminPageSizePrice eventBus={eventBus}></AdminPageSizePrice>
-          </Grid>
-        ) : null}
-        {status && status === 'Product' ? (
-          <Grid mt={2}>
-            <AdminPageProduct eventBus={eventBus}></AdminPageProduct>
-          </Grid>
-        ) : null}
-        {status && status === 'Orders' ? (
-          <Grid mt={2}>
-            <AdminPageOrders eventBus={eventBus}></AdminPageOrders>
-          </Grid>
-        ) : null}
-        {status && status === 'Menu' ? (
-          <Grid mt={2}>
-            <AdminMenu eventBus={eventBus}></AdminMenu>
-          </Grid>
-        ) : null}
-        {status && status === 'OrderByDate' ? (
-          <Grid mt={2}>
-            <AdminPageOrderByDate eventBus={eventBus}></AdminPageOrderByDate>
-          </Grid>
-        ) : null}
-        {status && status === 'GnCategory' ? (
-          <Grid mt={2}>
-            <GnCategory eventBus={eventBus}></GnCategory>
-          </Grid>
-        ) : null}
-        {status && status === 'GnProduct' ? (
-          <Grid mt={2}>
-            <GnProduct eventBus={eventBus}></GnProduct>
-          </Grid>
-        ) : null}
-        {status && status === 'GetOrdersByData' ? (
-          <Grid mt={2}>
-            <AdminPageGetOrdersByData
-              eventBus={eventBus}
-            ></AdminPageGetOrdersByData>
-          </Grid>
-        ) : null}
-        {status && status === 'GetCloudinaryDetails' ? (
-          <Grid mt={2}>
-            <h1>פרטי התחברות :</h1>
-            <a
-              title="link"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://cloudinary.com/users/login"
-            >
-              קישור
-            </a>
-            <h4>שם משתמש : arigabay199875@gmail.com </h4>
-          </Grid>
-        ) : null}
-      </Grid>
-    )
+    <>
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      {Object.keys(userAuth).length && (
+        <Grid mt={2} mr={2}>
+          <Button
+            className={classes.marginLeft}
+            text="קטגוריה"
+            onClick={() => handleClick('Category')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="מחירון"
+            onClick={() => handleClick('Price')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="מחיר למחירון"
+            onClick={() => handleClick('SizePrice')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="מוצר"
+            onClick={() => handleClick('Product')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="הזמנות"
+            onClick={() => handleClick('Orders')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="נעילת תפריט"
+            onClick={() => handleClick('Menu')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="הזמנות על פי תאריך"
+            onClick={() => handleClick('GetOrdersByData')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="הזמנות לתאריך ספציפי"
+            onClick={() => handleClick('OrderByDate')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="קטגוריות - גני איילון"
+            onClick={() => handleClick('GnCategory')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="מוצר - גני איילון"
+            onClick={() => handleClick('GnProduct')}
+          />
+          <Button
+            className={classes.marginLeft}
+            text="ענן תמונות"
+            onClick={() => handleClick('GetCloudinaryDetails')}
+          />
+          {status && status === 'Category' ? (
+            <Grid mt={2}>
+              <AdminPageCategory eventBus={eventBus}></AdminPageCategory>
+            </Grid>
+          ) : null}
+          {status && status === 'Price' ? (
+            <Grid mt={2}>
+              <AdminPagePrice eventBus={eventBus}></AdminPagePrice>
+            </Grid>
+          ) : null}
+          {status && status === 'SizePrice' ? (
+            <Grid mt={2}>
+              <AdminPageSizePrice eventBus={eventBus}></AdminPageSizePrice>
+            </Grid>
+          ) : null}
+          {status && status === 'Product' ? (
+            <Grid mt={2}>
+              <AdminPageProduct eventBus={eventBus}></AdminPageProduct>
+            </Grid>
+          ) : null}
+          {status && status === 'Orders' ? (
+            <Grid mt={2}>
+              <AdminPageOrders eventBus={eventBus}></AdminPageOrders>
+            </Grid>
+          ) : null}
+          {status && status === 'Menu' ? (
+            <Grid mt={2}>
+              <AdminMenu eventBus={eventBus}></AdminMenu>
+            </Grid>
+          ) : null}
+          {status && status === 'OrderByDate' ? (
+            <Grid mt={2}>
+              <AdminPageOrderByDate eventBus={eventBus}></AdminPageOrderByDate>
+            </Grid>
+          ) : null}
+          {status && status === 'GnCategory' ? (
+            <Grid mt={2}>
+              <GnCategory eventBus={eventBus}></GnCategory>
+            </Grid>
+          ) : null}
+          {status && status === 'GnProduct' ? (
+            <Grid mt={2}>
+              <GnProduct eventBus={eventBus}></GnProduct>
+            </Grid>
+          ) : null}
+          {status && status === 'GetOrdersByData' ? (
+            <Grid mt={2}>
+              <AdminPageGetOrdersByData
+                eventBus={eventBus}
+              ></AdminPageGetOrdersByData>
+            </Grid>
+          ) : null}
+          {status && status === 'GetCloudinaryDetails' ? (
+            <Grid mt={2}>
+              <h1>פרטי התחברות :</h1>
+              <a
+                title="link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://cloudinary.com/users/login"
+              >
+                קישור
+              </a>
+              <h4>שם משתמש : arigabay199875@gmail.com </h4>
+            </Grid>
+          ) : null}
+        </Grid>
+      )}
+    </>
   );
 };
