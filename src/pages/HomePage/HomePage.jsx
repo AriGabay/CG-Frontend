0ece50a8e10d120590f7879f032e875be1e1e325
AriGabay <arigabay19988@gmail.com>
2025-03-24 08:24:27 +0200

    try again

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -159,6 +159,7 @@ export const HomePage = () => {
               text="לתפריט פסח"
             />
           )}
+          
           {isMenuEnablesLoaded && menuEnables['tishray'] && (
             <BackButton
               classProp="menuButton"
