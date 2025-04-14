ec46d844ecb9634656bf4dd5b01518a832435b18
AriGabay <arigabay19988@gmail.com>
2025-04-14 12:14:52 +0300

    Update PdfViewerPopup.jsx

--- src/cmps/PdfViewerPopup/PdfViewerPopup.jsx
+++ src/cmps/PdfViewerPopup/PdfViewerPopup.jsx
@@ -6,7 +6,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import pdfFile from './Menu_Peacha_2024.pdf';
+import pdfFile from './Menu_Peacha_2025.pdf';
 
 const customTheme = getCustomTheme();
 
